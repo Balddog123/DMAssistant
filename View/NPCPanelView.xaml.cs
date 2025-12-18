332c8de08a5dec0051cbdd229d42936e9715f9e7
David <shurtleff123.ds@gmail.com>
2025-12-18 16:14:04 -0700

    changes npc panel view to allow sorting by name and home

--- View/NPCPanelView.xaml.cs
+++ View/NPCPanelView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,35 @@ namespace DMAssistant.View
     /// </summary>
     public partial class NPCPanelView : UserControl
     {
+        private TextBlock _lastBlockClicked;
+        private ListSortDirection _lastDirection;
+
         public NPCPanelView()
         {
             InitializeComponent();
+        }
+
+        private void NPCListView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is not TextBlock block) return;
+
+            var sortBy = block.Text;
+
+            if (sortBy.ToLower() == "home") sortBy = "Home.Name";
+            else if (sortBy.ToLower() == "name") sortBy = "Name";
+            else return;
+
+            ListSortDirection direction = ListSortDirection.Ascending;
+            if (block == _lastBlockClicked && _lastDirection == ListSortDirection.Ascending)
+                direction = ListSortDirection.Descending;
+
+            ICollectionView view = CollectionViewSource.GetDefaultView(NPCListView.ItemsSource);
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription(sortBy, direction));
+            view.Refresh();
+
+            _lastBlockClicked = block;
+            _lastDirection = direction;
         }
     }
 }
