1145b24b644331bbadcf98bc2604b65452017dbe
David <shurtleff123.ds@gmail.com>
2025-12-30 00:24:30 -0700

    adds beginning of layers to map editor. working on layer viewer

--- Model/Map.cs
+++ Model/Map.cs
@@ -1,191 +1,109 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Ink;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Linq;
 
 namespace DMAssistant.Model
 {
     public class Map : ObservableObject
     {
-        private byte[] _strokeData;
-        public byte[] StrokeData { get => _strokeData; set
-            {
-                if (SetProperty(ref _strokeData, value))
-                {
-                    // Tell UI that ThumbnailData also changed
-                    OnPropertyChanged(nameof(ThumbnailData));
-                    OnPropertyChanged(nameof(FullThumbnailData));
-                }
-            }
+        private ObservableCollection<InkLayerData> _layers = new();
+        public ObservableCollection<InkLayerData> Layers
+        {
+            get => _layers;
+            set => SetProperty(ref _layers, value);
         }
 
         private ObservableCollection<NoteBoxData> _noteBoxes;
-        public ObservableCollection<NoteBoxData> NoteBoxes { get => _noteBoxes; set => SetProperty(ref _noteBoxes, value); }
-
-        public byte[]? ThumbnailData
+        public ObservableCollection<NoteBoxData> NoteBoxes
         {
-            get
-            {
-                if (StrokeData == null || StrokeData.Length == 0)
-                {
-                    // Generate a placeholder thumbnail
-                    int width = 100;
-                    int height = 100;
-
-                    var rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
-                    var dv = new DrawingVisual();
-
-                    using (var dc = dv.RenderOpen())
-                    {
-                        dc.DrawRectangle(Brushes.LightGray, null, new Rect(0, 0, width, height));
-                        dc.DrawLine(new Pen(Brushes.DarkGray, 1), new Point(0, 0), new Point(width, height));
-                        dc.DrawLine(new Pen(Brushes.DarkGray, 1), new Point(0, height), new Point(width, 0));
-                    }
-
-                    rtb.Render(dv);
-
-                    var encoder = new PngBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(rtb));
-
-                    using (var ms = new MemoryStream())
-                    {
-                        encoder.Save(ms);
-                        return ms.ToArray();
-                    }
-                }
-                else
-                {
-                    // Recreate StrokeCollection from stored StrokeData
-                    StrokeCollection strokes;
-                    using (var ms = new MemoryStream(StrokeData))
-                    {
-                        strokes = new StrokeCollection(ms);
-                    }
-
-                    // Original canvas size (use the actual InkCanvas size)
-                    double originalWidth = 2000;
-                    double originalHeight = 2000;
-
-                    // Thumbnail size
-                    double thumbWidth = 100;
-                    double thumbHeight = 100;
-
-                    // Compute scaling factors
-                    double scaleX = thumbWidth / originalWidth;
-                    double scaleY = thumbHeight / originalHeight;
-
-                    var rtb = new RenderTargetBitmap((int)thumbWidth, (int)thumbHeight, 96, 96, PixelFormats.Pbgra32);
-
-                    var dv = new DrawingVisual();
-                    using (var dc = dv.RenderOpen())
-                    {
-                        // Draw background
-                        dc.DrawRectangle(Brushes.LightGray, null, new System.Windows.Rect(0, 0, thumbWidth, thumbHeight));
-
-                        // Apply scale transform so strokes shrink to fit
-                        dc.PushTransform(new ScaleTransform(scaleX, scaleY));
-
-                        // Draw strokes
-                        strokes.Draw(dc);
-
-                        dc.Pop(); // remove scaling transform
-                    }
-
-                    rtb.Render(dv);
-
-                    // Convert to byte[] using PNG
-                    var encoder = new PngBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(rtb));
-                    using (var ms = new MemoryStream())
-                    {
-                        encoder.Save(ms);
-                        return ms.ToArray();
-                    }
-                }
-
-                
-            }
+            get => _noteBoxes;
+            set => SetProperty(ref _noteBoxes, value);
         }
-        public byte[]? FullThumbnailData
-        {
-            get
-            {
-                if (StrokeData == null || StrokeData.Length == 0)
-                {
-                    // Generate a placeholder thumbnail
-                    int width = 2000;
-                    int height = 2000;
 
-                    var rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
-                    var dv = new DrawingVisual();
+        // Convenience properties: composite all visible layers into a thumbnail
+        public byte[]? ThumbnailData => RenderThumbnail(100, 100);
 
-                    using (var dc = dv.RenderOpen())
-                    {
-                        dc.DrawRectangle(Brushes.LightGray, null, new Rect(0, 0, width, height));
-                        dc.DrawLine(new Pen(Brushes.DarkGray, 1), new Point(0, 0), new Point(width, height));
-                        dc.DrawLine(new Pen(Brushes.DarkGray, 1), new Point(0, height), new Point(width, 0));
-                    }
-
-                    rtb.Render(dv);
-
-                    var encoder = new PngBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(rtb));
-
-                    using (var ms = new MemoryStream())
-                    {
-                        encoder.Save(ms);
-                        return ms.ToArray();
-                    }
-                }
-                else
-                {
-                    // Recreate StrokeCollection from stored StrokeData
-                    StrokeCollection strokes;
-                    using (var ms = new MemoryStream(StrokeData))
-                    {
-                        strokes = new StrokeCollection(ms);
-                    }
-
-                    // Original canvas size (use the actual InkCanvas size)
-                    double originalWidth = 2000;
-                    double originalHeight = 2000;
-
-                    var rtb = new RenderTargetBitmap((int)originalWidth, (int)originalHeight, 96, 96, PixelFormats.Pbgra32);
-
-                    var dv = new DrawingVisual();
-                    using (var dc = dv.RenderOpen())
-                    {
-                        // Draw background
-                        dc.DrawRectangle(Brushes.LightGray, null, new System.Windows.Rect(0, 0, originalWidth, originalHeight));
-
-                        // Draw strokes
-                        strokes.Draw(dc);
-                    }
-
-                    rtb.Render(dv);
-
-                    // Convert to byte[] using PNG
-                    var encoder = new PngBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(rtb));
-                    using (var ms = new MemoryStream())
-                    {
-                        encoder.Save(ms);
-                        return ms.ToArray();
-                    }
-                }
-
-
-            }
-        }
+        public byte[]? FullThumbnailData => RenderThumbnail(2000, 2000);
 
         public Map()
-        {            
+        {
             NoteBoxes = new ObservableCollection<NoteBoxData>();
         }
 
+        /// <summary>
+        /// Renders all visible layers into a bitmap of the specified size.
+        /// </summary>
+        private byte[] RenderThumbnail(double targetWidth, double targetHeight)
+        {
+            if (Layers == null || Layers.Count == 0 || Layers.All(l => l.StrokeData == null || l.StrokeData.Length == 0))
+            {
+                // Return placeholder image
+                var rtb = new RenderTargetBitmap((int)targetWidth, (int)targetHeight, 96, 96, PixelFormats.Pbgra32);
+                var dv = new DrawingVisual();
+                using (var dc = dv.RenderOpen())
+                {
+                    dc.DrawRectangle(Brushes.LightGray, null, new Rect(0, 0, targetWidth, targetHeight));
+                    dc.DrawLine(new Pen(Brushes.DarkGray, 1), new Point(0, 0), new Point(targetWidth, targetHeight));
+                    dc.DrawLine(new Pen(Brushes.DarkGray, 1), new Point(0, targetHeight), new Point(targetWidth, 0));
+                }
+                rtb.Render(dv);
+
+                using var ms = new MemoryStream();
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+                encoder.Save(ms);
+                return ms.ToArray();
+            }
+
+            // Assume original canvas size
+            double originalWidth = 2000;
+            double originalHeight = 2000;
+
+            // Compute scaling factors for the thumbnail
+            double scaleX = targetWidth / originalWidth;
+            double scaleY = targetHeight / originalHeight;
+
+            var rtbBitmap = new RenderTargetBitmap((int)targetWidth, (int)targetHeight, 96, 96, PixelFormats.Pbgra32);
+            var dvComposite = new DrawingVisual();
+            using (var dc = dvComposite.RenderOpen())
+            {
+                // Background
+                dc.DrawRectangle(Brushes.LightGray, null, new Rect(0, 0, targetWidth, targetHeight));
+
+                // Draw all visible layers
+                dc.PushTransform(new ScaleTransform(scaleX, scaleY));
+                foreach (var layer in Layers)
+                {
+                    if (!layer.IsVisible || layer.StrokeData == null || layer.StrokeData.Length == 0)
+                        continue;
+
+                    var strokes = new StrokeCollection(new MemoryStream(layer.StrokeData));
+                    strokes.Draw(dc);
+                }
+                dc.Pop();
+            }
+
+            rtbBitmap.Render(dvComposite);
+
+            using var output = new MemoryStream();
+            var pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtbBitmap));
+            pngEncoder.Save(output);
+            return output.ToArray();
+        }
+    }
+
+    public class InkLayerData
+    {
+        public string Name { get; set; } = "Layer";
+        public byte[] StrokeData { get; set; } = new byte[0];   // Serialized as Base64
+        public bool IsVisible { get; set; } = true;
+        public bool IsLocked { get; set; } = false;
+        public double Opacity { get; set; } = 1.0;
     }
 }
--- View/MapWindow.xaml.cs
+++ View/MapWindow.xaml.cs
@@ -1,26 +1,18 @@
 ﻿using DMAssistant.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DMAssistant.View
 {
-    /// <summary>
-    /// Interaction logic for MapWindow.xaml
-    /// </summary>
     public partial class MapWindow : Window
     {
         private bool _snapToGrid = false;
@@ -28,31 +20,75 @@ namespace DMAssistant.View
         private NoteBox currentNoteBox;
         private Point startPoint;
 
-        //moving the notebox
         private bool _isDraggingNote = false;
         private Point _dragStartPoint;
         private NoteBox _draggingNote;
 
-        Map _map;
+        // Zoom
+        private double _zoom = 1.0;
+        private const double ZoomStep = 0.1;
+        private const double MinZoom = 0.25;
+        private const double MaxZoom = 4.0;
 
+        private Map _map;
+        private InkLayerData _activeLayer => _map.Layers[0]; // for now, last added layer
+        private InkCanvas _activeInkCanvas => _activeLayer != null ? _layerCanvases[_activeLayer] : null;
+
+        public static readonly RoutedCommand IncreaseStrokeSizeCommand = new();
+        public static readonly RoutedCommand DecreaseStrokeSizeCommand = new();
+
+
+        // Dictionary mapping each InkLayerData to its InkCanvas instance
+        private Dictionary<InkLayerData, InkCanvas> _layerCanvases = new();
 
         public MapWindow(Map map)
         {
             _map = map;
             InitializeComponent();
-            Ink.StrokeCollected += Ink_StrokeCollected;
 
-            LoadImageData();
+            LoadLayers();
+            LoadNoteBoxes();
         }
 
-        private void SaveCanvas(object sender, RoutedEventArgs e)
+        #region Layer Management
+
+        /// <summary>
+        /// Loads all layers from the map and creates InkCanvas instances dynamically
+        /// </summary>
+        private void LoadLayers()
         {
-            // Save InkCanvas strokes
-            using (var ms = new MemoryStream())
+            if (_map.Layers.Count == 0)
             {
-                Ink.Strokes.Save(ms);
-                _map.StrokeData = ms.ToArray();
+                _map.Layers.Add(new InkLayerData());
             }
+
+            foreach (var layer in _map.Layers)
+            {
+                CreateInkCanvasForLayer(layer);
+            }
+
+            Debug.WriteLine(_activeLayer == null);
+        }
+        private void SaveMap(object sender, RoutedEventArgs e)
+        {
+            if (_map.Layers == null) _map.Layers = new ObservableCollection<InkLayerData>();
+
+            _map.Layers.Clear();
+
+            foreach (var layer in _layerCanvases.Keys)
+            {
+                var canvas = _layerCanvases[layer];
+
+                using (var ms = new MemoryStream())
+                {
+                    canvas.Strokes.Save(ms);
+
+                    layer.StrokeData = ms.ToArray(); // save strokes to layer object
+                }
+
+                _map.Layers.Add(layer); // add layer to map's list
+            }
+
 
             // Save NoteBoxes
             _map.NoteBoxes.Clear();
@@ -71,37 +107,202 @@ namespace DMAssistant.View
                     });
                 }
             }
-
-            MessageBox.Show("Map saved successfully!");
         }
 
-
-
-        private void LoadImageData()
+        /// <summary>
+        /// Creates an InkCanvas for the given layer and adds it to the container
+        /// </summary>
+        private void CreateInkCanvasForLayer(InkLayerData layer)
         {
-            // Load strokes
-            if (_map.StrokeData != null && _map.StrokeData.Length > 0)
+            var ink = new InkCanvas
             {
-                using (var ms = new MemoryStream(_map.StrokeData))
-                {
-                    Ink.Strokes = new StrokeCollection(ms);
-                }
+                Width = 2000,
+                Height = 2000,
+                Background = Brushes.Transparent,
+                IsHitTestVisible = !layer.IsLocked,
+                Opacity = layer.Opacity
+            };
+
+            if (layer.StrokeData != null && layer.StrokeData.Length > 0)
+            {
+                using var ms = new MemoryStream(layer.StrokeData);
+                ink.Strokes = new StrokeCollection(ms);
             }
 
-            // Load NoteBoxes
+            ink.StrokeCollected += (s, e) =>
+            {
+                if (_snapToGrid)
+                {
+                    SnapStrokeToGrid(e.Stroke);
+                }
+                SaveLayerStrokes(layer, ink);
+            };
+
+            // Insert into container at correct Z-index (top = last)
+            LayerContainer.Children.Add(ink);
+            _layerCanvases[layer] = ink;
+        }
+
+        /// <summary>
+        /// Saves the strokes from an InkCanvas back to the layer
+        /// </summary>
+        private void SaveLayerStrokes(InkLayerData layer, InkCanvas ink)
+        {
+            using var ms = new MemoryStream();
+            ink.Strokes.Save(ms);
+            layer.StrokeData = ms.ToArray();            
+        }
+
+        /// <summary>
+        /// Snaps a stroke to the grid
+        /// </summary>
+        private void SnapStrokeToGrid(Stroke stroke)
+        {
+            var snappedPoints = new StylusPointCollection();
+            foreach (var p in stroke.StylusPoints)
+            {
+                double x = Math.Round(p.X / gridSize) * gridSize;
+                double y = Math.Round(p.Y / gridSize) * gridSize;
+                snappedPoints.Add(new StylusPoint(x, y));
+            }
+            stroke.StylusPoints = snappedPoints;
+        }
+
+        /// <summary>
+        /// Adds a new layer to the map and UI
+        /// </summary>
+        private InkLayerData AddNewLayer(string name = "New Layer")
+        {
+            var layer = new InkLayerData { Name = name };
+            _map.Layers.Add(layer);
+            CreateInkCanvasForLayer(layer);
+            return layer;
+        }
+
+        /// <summary>
+        /// Removes a layer
+        /// </summary>
+        private void RemoveLayer(InkLayerData layer)
+        {
+            if (_layerCanvases.TryGetValue(layer, out var ink))
+            {
+                LayerContainer.Children.Remove(ink);
+                _layerCanvases.Remove(layer);
+            }
+            _map.Layers.Remove(layer);
+        }
+
+        /// <summary>
+        /// Moves a layer up in Z-order
+        /// </summary>
+        private void MoveLayerUp(InkLayerData layer)
+        {
+            if (!_layerCanvases.TryGetValue(layer, out var ink)) return;
+
+            int index = LayerContainer.Children.IndexOf(ink);
+            if (index < LayerContainer.Children.Count - 1)
+            {
+                LayerContainer.Children.RemoveAt(index);
+                LayerContainer.Children.Insert(index + 1, ink);
+
+                // Update model ordering
+                int modelIndex = _map.Layers.IndexOf(layer);
+                _map.Layers.RemoveAt(modelIndex);
+                _map.Layers.Insert(modelIndex + 1, layer);
+            }
+        }
+
+        /// <summary>
+        /// Moves a layer down in Z-order
+        /// </summary>
+        private void MoveLayerDown(InkLayerData layer)
+        {
+            if (!_layerCanvases.TryGetValue(layer, out var ink)) return;
+
+            int index = LayerContainer.Children.IndexOf(ink);
+            if (index > 0)
+            {
+                LayerContainer.Children.RemoveAt(index);
+                LayerContainer.Children.Insert(index - 1, ink);
+
+                int modelIndex = _map.Layers.IndexOf(layer);
+                _map.Layers.RemoveAt(modelIndex);
+                _map.Layers.Insert(modelIndex - 1, layer);
+            }
+        }
+
+        /// <summary>
+        /// Sets layer visibility
+        /// </summary>
+        private void SetLayerVisibility(InkLayerData layer, bool visible)
+        {
+            layer.IsVisible = visible;
+            if (_layerCanvases.TryGetValue(layer, out var ink))
+            {
+                ink.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// Sets layer locked state
+        /// </summary>
+        private void SetLayerLocked(InkLayerData layer, bool locked)
+        {
+            layer.IsLocked = locked;
+            if (_layerCanvases.TryGetValue(layer, out var ink))
+            {
+                ink.IsHitTestVisible = !locked;
+            }
+        }
+
+        /// <summary>
+        /// Sets layer opacity
+        /// </summary>
+        private void SetLayerOpacity(InkLayerData layer, double opacity)
+        {
+            layer.Opacity = opacity;
+            if (_layerCanvases.TryGetValue(layer, out var ink))
+            {
+                ink.Opacity = opacity;
+            }
+        }
+
+        #endregion
+
+        #region Zoom Handling
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl))
+                return;
+
+            _zoom += e.Delta > 0 ? ZoomStep : -ZoomStep;
+            _zoom = Math.Clamp(_zoom, MinZoom, MaxZoom);
+
+            ZoomTransform.ScaleX = _zoom;
+            ZoomTransform.ScaleY = _zoom;
+
+            e.Handled = true;
+        }
+
+        #endregion
+
+        #region NoteBoxes (unchanged)
+
+        private void LoadNoteBoxes()
+        {
             foreach (var noteData in _map.NoteBoxes)
             {
-                var note = new NoteBox(noteData.Width, noteData.Height);
-                note.Width = noteData.Width;
-                //note.Height = noteData.Height;
-                note.Text = noteData.Text;
-                note.Title = noteData.Title;
-                
+                var note = new NoteBox(noteData.Width, noteData.Height)
+                {
+                    Width = noteData.Width,
+                    Text = noteData.Text,
+                    Title = noteData.Title
+                };
 
                 Canvas.SetLeft(note, noteData.X);
                 Canvas.SetTop(note, noteData.Y);
 
-                //Capture note box move
                 note.MouseLeftButtonDown += NoteBox_MouseLeftButtonDown;
                 note.MouseMove += NoteBox_MouseMove;
                 note.MouseLeftButtonUp += NoteBox_MouseLeftButtonUp;
@@ -114,59 +315,9 @@ namespace DMAssistant.View
                 NoteBoxes.Children.Add(note);
             }
         }
-
-        private void EnableGridSnapping(object sender, RoutedEventArgs e)
-        {
-            Ink.EditingMode = InkCanvasEditingMode.Ink;
-            _snapToGrid = true;
-        }
-
-        private void DisableGridSnapping(object sender, RoutedEventArgs e)
-        {
-            Ink.EditingMode = InkCanvasEditingMode.Ink;
-            _snapToGrid = false;
-        }
-
-        private void EnableErasing(object sender, RoutedEventArgs e)
-        {
-            Ink.EditingMode = InkCanvasEditingMode.EraseByStroke;
-            _snapToGrid = false;
-        }
-        private void EnablePointer(object sender, RoutedEventArgs e)
-        {
-            Ink.EditingMode = InkCanvasEditingMode.Select;
-            _snapToGrid = false;
-        }
-
-
-        private void Ink_StrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
-        {
-            if (!_snapToGrid)
-                return;
-
-            Stroke stroke = e.Stroke;
-
-            StylusPointCollection snappedPoints = new StylusPointCollection();
-
-            foreach (var p in stroke.StylusPoints)
-            {
-                double x = Math.Round(p.X / gridSize) * gridSize;
-                double y = Math.Round(p.Y / gridSize) * gridSize;
-
-                snappedPoints.Add(new StylusPoint(x, y));
-            }
-
-            stroke.StylusPoints = snappedPoints;
-        }
-
-        private void DrawNoteButton_Click(object sender, RoutedEventArgs e)
-        {
-            Ink.EditingMode = InkCanvasEditingMode.None;
-            Ink.MouseLeftButtonDown += InkCanvas_MouseDownForNote;
-        }
-
         private void InkCanvas_MouseDownForNote(object sender, MouseButtonEventArgs e)
         {
+            if (_activeInkCanvas == null) return;
             // Translate mouse position to NoteBoxes coordinates
             startPoint = e.GetPosition(NoteBoxes);
 
@@ -198,9 +349,9 @@ namespace DMAssistant.View
 
             e.Handled = true; // prevent InkCanvas from processing it
         }
-
         private void NoteBoxes_MouseMoveForNote(object sender, MouseEventArgs e)
         {
+            if (_activeInkCanvas == null) return;
             if (currentNoteBox == null || e.LeftButton != MouseButtonState.Pressed) return;
 
             // Translate position to NoteBoxes coordinates
@@ -218,10 +369,11 @@ namespace DMAssistant.View
 
         private void NoteBoxes_MouseUpForNote(object sender, MouseButtonEventArgs e)
         {
+            if (_activeInkCanvas == null) return;
             NoteBoxes.MouseMove -= NoteBoxes_MouseMoveForNote;
             NoteBoxes.MouseLeftButtonUp -= NoteBoxes_MouseUpForNote;
             NoteBoxes.ReleaseMouseCapture();
-            Ink.MouseLeftButtonDown -= InkCanvas_MouseDownForNote;
+            _activeInkCanvas.MouseLeftButtonDown -= InkCanvas_MouseDownForNote;
             currentNoteBox = null;
         }
 
@@ -265,6 +417,59 @@ namespace DMAssistant.View
             _isDraggingNote = false;
         }
 
+        #endregion
+        #region Controls
+        private void EnablePointer_Click(object sender, RoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void EnableErasing_Click(object sender, RoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void EnableGridSnapping_Click(object sender, RoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            _snapToGrid = true;
+        }
+
+        private void DisableGridSnapping_Click(object sender, RoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            _snapToGrid = false;
+        }
+
+        private void ToggleLayers_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void IncreaseStrokeSize_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.DefaultDrawingAttributes.Height++;
+            _activeInkCanvas.DefaultDrawingAttributes.Width++;
+        }
+        private void DecreaseStrokeSize_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.DefaultDrawingAttributes.Height--;
+            _activeInkCanvas.DefaultDrawingAttributes.Width--;
+        }
+        private void DrawNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_activeInkCanvas == null) return;
+            _activeInkCanvas.EditingMode = InkCanvasEditingMode.None;
+            _activeInkCanvas.MouseLeftButtonDown += InkCanvas_MouseDownForNote;
+        }
+        
+        #endregion
 
     }
 }
