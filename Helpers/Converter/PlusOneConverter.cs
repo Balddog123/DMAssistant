ae0598d5738afd5ae1724374530f0ca436315ccb
David <shurtleff123.ds@gmail.com>
2026-02-10 15:28:39 -0700

    fixes the displayed index for session secrets and tables

---
+++ Helpers/Converter/PlusOneConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace DMAssistant.Helpers.Converter
+{
+    public class PlusOneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int i ? i + 1 : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+
+}
